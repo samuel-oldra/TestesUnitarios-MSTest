18312ae963aad0cba38484e674aa71197c5d5fe6
Samuel B. Oldra <samuel.oldra@gmail.com>
2010-04-01 19:00:00 -0300

    Primeiro commit

---
+++ ProjetoExemplo/FuncMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetoExemplo
+{
+    public class FuncMath
+    {
+        public static int Divide(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+
+        public static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Soma(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
